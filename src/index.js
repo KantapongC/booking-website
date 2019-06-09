774d20f1dd523660e2855faefb84ecf13852554e
Kantapong Chanthanawan <yeem.kantapong@gmail.com>
2019-06-10 01:58:01 +0700

    Adding react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Navbar from './components/Navbar/Navbar';
+import Home from './views/Home/Home';
+import About from './views/About/About';
+import Booking from './views/Booking/Booking';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+ReactDOM.render(
+	<Router>
+		<Navbar />
+		<Switch>
+			<Route exact path='/' component={Home} />
+			<Route path='/about' component={About} />
+			<Route path='/booking' component={Booking} />
+		</Switch>
+	</Router>,
+	document.getElementById('root')
+);
 
 serviceWorker.unregister();
