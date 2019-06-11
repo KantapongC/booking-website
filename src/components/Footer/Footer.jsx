c5465473b65fcfc84600ddb736b0935b68b1ccb9
Kantapong Chanthanawan <yeem.kantapong@gmail.com>
2019-06-12 01:54:29 +0700

    Adding footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Layout } from 'antd';
+
+const Footer = () => {
+	return <Layout.Footer style={{ textAlign: 'center' }}>Mol Salon ©1996 Created by Kantapong Chanthanawan</Layout.Footer>;
+};
+
+export default Footer;
