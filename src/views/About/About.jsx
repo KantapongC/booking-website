dc8c7d1c313af1f0178624f90cd41373c098a7af
Kantapong Chanthanawan <yeem.kantapong@gmail.com>
2019-06-17 01:18:55 +0700

    Adding home page with images and content

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -4,7 +4,7 @@ import { Layout } from 'antd';
 export default class About extends Component {
 	render() {
 		return (
-			<Layout.Content style={{ padding: '25px' }}>
+			<Layout.Content>
 				<div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
 					<h1 style={{ 'text-align': 'center' }}>This is a About page</h1>
 				</div>
