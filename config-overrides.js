f67c05e9fc4d88fbf88d565a7980478603a87b1c
Kantapong Chanthanawan <yeem.kantapong@gmail.com>
2019-06-21 00:37:07 +0700

    Updating Home page responsive

--- config-overrides.js
+++ config-overrides.js
@@ -9,7 +9,12 @@ module.exports = override(
 	}),
 	addLessLoader({
 		javascriptEnabled: true,
-		modifyVars: { '@primary-color': '#7f8c8d' }
+		modifyVars: {
+			'@primary-color': '#7f8c8d',
+			'@layout-header-background': '#ffffff',
+			'@layout-header-height': '90px',
+			'@layout-header-padding': '0px'
+		}
 	})
 );
 
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -13,43 +13,44 @@ import img_8 from './img/img_8.jpg';
 export default class Home extends Component {
 	render() {
 		return (
-			<Layout.Content>
+			<Carousel autoplay autoplaySpeed={4000}>
 				{/* <Carousel> */}
-				<Carousel autoplay autoplaySpeed={4000}>
-					<div>
-						<img src={img_5} alt='Display 1' />
-						<div className='content'>
-							<div className='title'>Lorem ipsum dolor</div>
-							<div className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, assumenda</div>
+				{/* <Layout.Content> */}
+				<div>
+					<img src={img_5} alt='Display 1' />
+					<div className='content'>
+						<div className='title'>Lorem ipsum dolor</div>
+						<div className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, assumenda</div>
+					</div>
+				</div>
+				<div>
+					<img src={img_6} alt='Display 2' />
+					<div className='content'>
+						<div className='title'>Et adipisci ratione </div>
+						<div className='text'>necessitatibus soluta similique architecto veritatis pariatur autem fugiat</div>
+					</div>
+				</div>
+				<div>
+					<img src={img_7} alt='Display 3' />
+					<div className='content'>
+						<div className='title'>inventore adipisci odit</div>
+						<div className='text'>
+							corrupti architecto quas quo repudiandae natus cumque nostrum sunt inventore commodi
 						</div>
 					</div>
-					<div>
-						<img src={img_6} alt='Display 2' />
-						<div className='content'>
-							<div className='title'>Et adipisci ratione </div>
-							<div className='text'>necessitatibus soluta similique architecto veritatis pariatur autem fugiat</div>
+				</div>
+				<div>
+					<img src={img_8} alt='Display 4' />
+					<div className='content'>
+						<div className='title'>aspernatur impedit</div>
+						<div className='text'>
+							illum quasi cumque maxime ullam assumenda alias, quae, recusandae consequuntur a natus. Quasi repudiandae
+							magni reiciendis
 						</div>
 					</div>
-					<div>
-						<img src={img_7} alt='Display 3' />
-						<div className='content'>
-							<div className='title'>inventore adipisci odit</div>
-							<div className='text'>
-								corrupti architecto quas quo repudiandae natus cumque nostrum sunt inventore commodi
-							</div>
-						</div>
-					</div>
-					<div>
-						<img src={img_8} alt='Display 4' />
-						<div className='content'>
-							<div className='title'>aspernatur impedit</div>
-							<div className='text'>
-								illum quasi cumque maxime ullam assumenda alias, quae, recusandae consequuntur a natus. Quasi repudiandae magni reiciendis
-							</div>
-						</div>
-					</div>
-				</Carousel>
-			</Layout.Content>
+				</div>
+			</Carousel>
+			// </Layout.Content>
 		);
 	}
 }
