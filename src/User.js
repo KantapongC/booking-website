2e4ed6553a6b581409f6f0fcbd9cadc0c4c7db9b
Kantapong Chanthanawan <yeem.kantapong@gmail.com>
2019-06-29 15:20:24 +0700

    Edit routing for admin and users

---
+++ src/User.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Layout } from 'antd';
+
+import Navbar from './components/Navbar/Navbar';
+import Dashboard from './views/Admin/Dashboard';
+import Home from './views/Home/Home';
+import About from './views/About/About';
+import PriceList from './views/PriceList/PriceList';
+import Staff from './views/Staff/Staff';
+import Contact from './views/Contact/Contact';
+import Booking from './views/Booking/Booking';
+import Footer from './components/Footer/Footer';
+
+export default class User extends Component {
+	render() {
+		return (
+			<div>
+				<Layout className='layout'>
+					<Layout.Header>
+						<Navbar />
+					</Layout.Header>
+				</Layout>
+				<Switch>
+					<Route path='/admin' component={Dashboard} />
+					<Route exact path='/' component={Home} />
+					<Route path='/about' component={About} />
+					<Route path='/pricelist' component={PriceList} />
+					<Route path='/staff' component={Staff} />
+					<Route path='/contact' component={Contact} />
+					<Route path='/booking' component={Booking} />
+				</Switch>
+				<Layout>
+					<Footer />
+				</Layout>
+			</div>
+		);
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,33 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './index.css';
-import Navbar from './components/Navbar/Navbar';
-import Home from './views/Home/Home';
-import About from './views/About/About';
-import PriceList from './views/PriceList/PriceList';
-import Staff from './views/Staff/Staff';
-import Contact from './views/Contact/Contact';
-import Booking from './views/Booking/Booking';
-import Footer from './components/Footer/Footer';
 import * as serviceWorker from './serviceWorker';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Layout } from 'antd';
+import Admin from './Admin';
+import User from './User';
 
 ReactDOM.render(
 	<Router>
-		<Navbar />
-		<Layout className='layout'>
-			<Switch>
-					<Route exact path='/' component={Home} />
-					<Route path='/about' component={About} />
-					<Route path='/pricelist' component={PriceList} />
-					<Route path='/staff' component={Staff} />
-					<Route path='/contact' component={Contact} />
-					<Route path='/booking' component={Booking} />
-			</Switch>
-			<Footer/>
-		</Layout>
+		<Switch>
+			<Route path='/admin' component={Admin} />
+			<Route exact path='/' component={User} />
+		</Switch>
 	</Router>,
 	document.getElementById('root')
 );
