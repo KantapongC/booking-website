3aaf9ddc59e60678b93e0e42481de135fa5304aa
Kantapong Chanthanawan <yeem.kantapong@gmail.com>
2020-01-12 14:19:35 +0700

    Adding acrions and reducer for employee settings

--- client/src/store/actions/employeeActions.js
+++ client/src/store/actions/employeeActions.js
@@ -1,19 +1,58 @@
 import axios from 'axios';
-import { CREATE_EMPLOYEE_SUCCESS, CREATE_EMPLOYEE_ERROR, GET_EMPLOYEE_SUCCESS, GET_EMPLOYEE_ERROR } from './types';
+import { setAlert } from '../actions/alert';
+import {
+  CREATE_EMPLOYEE_SUCCESS,
+  CREATE_EMPLOYEE_ERROR,
+  GET_EMPLOYEE_SUCCESS,
+  GET_EMPLOYEE_ERROR,
+  UPDATE_EMPLOYEE_SUCCESS,
+  UPDATE_EMPLOYEE_ERROR,
+  DELETE_EMPLOYEE_SUCCESS,
+  DELETE_EMPLOYEE_ERROR
+} from './types';
 
 export const createEmployee = employee => async dispatch => {
-	try {
-		dispatch({ type: CREATE_EMPLOYEE_SUCCESS, employee });
-	} catch (error) {
-		dispatch({ type: CREATE_EMPLOYEE_ERROR, error });
-	}
+  try {
+    const res = await axios.post('/api/employees/create', employee);
+
+    dispatch({ type: CREATE_EMPLOYEE_SUCCESS, payload: res.data });
+    dispatch(setAlert('การเพิ่มพนักงานสำเร็จ', 'success'));
+  } catch (error) {
+    dispatch({ type: CREATE_EMPLOYEE_ERROR, error });
+    dispatch(setAlert('การเพิ่มพนักงานไม่สำเร็จ', 'danger'));
+  }
 };
 
 export const getEmployee = () => async dispatch => {
-	try {
-		const res = await axios.get('/api/employees/');
-		dispatch({ type: GET_EMPLOYEE_SUCCESS, payload: res.data });
-	} catch (error) {
-		dispatch({ type: GET_EMPLOYEE_ERROR, error });
-	}
+  try {
+    const res = await axios.get('/api/employees/');
+
+    dispatch({ type: GET_EMPLOYEE_SUCCESS, payload: res.data });
+  } catch (error) {
+    dispatch({ type: GET_EMPLOYEE_ERROR, error });
+  }
+};
+
+export const updateEmployee = employee => async dispatch => {
+  try {
+    const res = await axios.put(`/api/employees/${employee._id}`, employee);
+
+    dispatch({ type: UPDATE_EMPLOYEE_SUCCESS, payload: res.data });
+    dispatch(setAlert('การแก้ไขพนักงานสำเร็จ', 'success'));
+  } catch (error) {
+    dispatch({ type: UPDATE_EMPLOYEE_ERROR, error });
+    dispatch(setAlert('การแก้ไขพนักงานไม่สำเร็จ', 'danger'));
+  }
+};
+
+export const deleteEmployee = employee => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/employees/${employee._id}`);
+
+    dispatch({ type: DELETE_EMPLOYEE_SUCCESS, payload: res.data });
+    dispatch(setAlert('การลบพนักงานสำเร็จ', 'success'));
+  } catch (error) {
+    dispatch({ type: DELETE_EMPLOYEE_ERROR, error });
+    dispatch(setAlert('การลบพนักงานไม่สำเร็จ', 'danger'));
+  }
 };
--- client/src/store/reducers/employeeReducer.js
+++ client/src/store/reducers/employeeReducer.js
@@ -1,19 +1,69 @@
-import { CREATE_EMPLOYEE_SUCCESS, CREATE_EMPLOYEE_ERROR, GET_EMPLOYEE_SUCCESS, GET_EMPLOYEE_ERROR } from '../actions/types';
+import {
+	CREATE_EMPLOYEE_SUCCESS,
+	CREATE_EMPLOYEE_ERROR,
+	GET_EMPLOYEE_SUCCESS,
+	GET_EMPLOYEE_ERROR,
+	UPDATE_EMPLOYEE_SUCCESS,
+	UPDATE_EMPLOYEE_ERROR,
+	DELETE_EMPLOYEE_SUCCESS,
+	DELETE_EMPLOYEE_ERROR
+} from '../actions/types';
 
 const INITIAL_STATE = {
-	error: null
+	error: null,
+	isLoading: false
 };
 
 const employeeReducer = (state = INITIAL_STATE, { type, payload, error }) => {
 	switch (type) {
 		case CREATE_EMPLOYEE_SUCCESS:
-			return state;
+			return {
+				...state,
+				isLoading: !state.isLoading
+			};
+
 		case CREATE_EMPLOYEE_ERROR:
-			return state;
+			return {
+				...state,
+				error
+			};
+
 		case GET_EMPLOYEE_SUCCESS:
-			return { ...state, employees: payload };
+			return {
+				...state,
+				employees: payload
+			};
+
 		case GET_EMPLOYEE_ERROR:
-			return { ...state, error };
+			return {
+				...state,
+				error
+			};
+
+		case UPDATE_EMPLOYEE_SUCCESS:
+			return {
+				...state,
+				isLoading: !state.isLoading
+			};
+
+		case UPDATE_EMPLOYEE_ERROR:
+			return {
+				...state,
+				error
+			};
+
+		case DELETE_EMPLOYEE_SUCCESS:
+			return {
+				...state,
+				isLoading: !state.isLoading
+			};
+
+		case DELETE_EMPLOYEE_ERROR:
+			return {
+				...state,
+				error
+			};
+
 		default:
 			return state;
 	}
